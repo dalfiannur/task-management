9967e83c414d3b7517f49b141179a1fb1a0eac87
Dikry Alfiannur <dalfiannur@gmail.com>
2026-07-29 00:16:54 +0700

    refactor(auth): remove OIDC provider from app root

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,27 +1,14 @@
-import { useEffect } from "react";
 import { RouterProvider } from "react-router";
-import { useAuth } from "react-oidc-context";
 import { ThemeProvider } from "next-themes";
-import { ApolloProvider, client, setAuthToken } from "@/lib/graphql-client";
+import { ApolloProvider, client } from "@/lib/graphql-client";
 import { Toaster } from "@/components/ui/sonner";
 import { router } from "./router";
-
-function InnerApp() {
-  const auth = useAuth();
-  const accessToken = auth.user?.access_token ?? null;
-
-  useEffect(() => {
-    setAuthToken(accessToken);
-  }, [accessToken]);
-
-  return <RouterProvider router={router} />;
-}
 
 export function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <ApolloProvider client={client}>
-        <InnerApp />
+        <RouterProvider router={router} />
         <Toaster />
       </ApolloProvider>
     </ThemeProvider>
